42f2344d787b07da2e5b0574945c1d99045c2103
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-17 03:47:51 +0900

    createview

---
+++ todo/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
+from .models import TodoModel	 
+
+class TodoList(ListView):
+	template_name = "list.html"
+	# 作成したデータをどのモデルに保存するのかを指定する
+	model = TodoModel
+	
+class TodoDetail(DetailView):
+	template_name = "detail.html"
+	model = TodoModel
+	
+class TodoCreate(CreateView):
+	template_name = "create.html"
+	model = TodoModel
+	fields = ("title", "memo", "priority", "duedate")
+	# lazyつけるとデータが保存されてから実行する（らしい
+	success_url = reverse_lazy("list")
