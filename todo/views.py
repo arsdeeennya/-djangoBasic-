960c043adab4e9d2c828878ca9a069ccb8d42df6
arsdeeennya <kkamazonkk@yahoo.co.jp>
2021-02-20 08:58:36 +0900

    delete機能追加

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
 from django.urls import reverse_lazy
 from .models import TodoModel	 
 
@@ -17,4 +17,9 @@ class TodoCreate(CreateView):
 	model = TodoModel
 	fields = ("title", "memo", "priority", "duedate")
 	# lazyつけるとデータが保存されてから実行する（らしい
+	success_url = reverse_lazy("list")
+	
+class TodoDelete(DeleteView):
+	template_name = "delete.html"
+	model = TodoModel
 	success_url = reverse_lazy("list")
